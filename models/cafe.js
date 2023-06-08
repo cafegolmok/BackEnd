0051d2e8a3a0d6dca0016b3542c6cb86aea96caa
Jihoon Chae <hoonn94@naver.com>
2023-06-08 16:50:21 +0900

    feat : cafes 모델 추가

--- models/cafe.js
+++ models/cafe.js
@@ -1,31 +1,35 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+// models/cafe.js
+
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Cafe extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
     }
   }
-  Cafe.init({
-    id: DataTypes.INTEGER,
-    cafeName: DataTypes.STRING,
-    starRating: DataTypes.DECIMAL,
-    location: DataTypes.STRING,
-    tags: DataTypes.STRING,
-    imgToUrl: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    information: DataTypes.STRING,
-    homepageUrl: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Cafe',
-  });
+  Cafe.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      cafeName: DataTypes.STRING,
+      starRating: DataTypes.DECIMAL,
+      location: DataTypes.STRING,
+      tags: DataTypes.STRING,
+      imgToUrl: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
+      information: DataTypes.STRING,
+      homepageUrl: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Cafe",
+      tableName: "Cafes",
+      timestamps: true,
+    }
+  );
   return Cafe;
 };
