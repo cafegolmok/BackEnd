b18498c4afa2161d0a167922482121b9e694f2e9
Jihoon Chae <hoonn94@naver.com>
2023-06-08 22:13:02 +0900

    feat : cafes 테이블 타입 추가

--- models/cafe.js
+++ models/cafe.js
@@ -1,35 +1,52 @@
-// models/cafe.js
+// models/Cafe.js
 
 "use strict";
-const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
   class Cafe extends Model {
-    
     static associate(models) {
+      // Define associations here
     }
   }
   Cafe.init(
     {
       id: {
         type: DataTypes.INTEGER,
-        autoIncrement: true,
         primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
       },
-      cafeName: DataTypes.STRING,
-      starRating: DataTypes.DECIMAL,
-      location: DataTypes.STRING,
-      tags: DataTypes.STRING,
-      imgToUrl: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      information: DataTypes.STRING,
-      homepageUrl: DataTypes.STRING,
+      cafeName: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      starRating: {
+        type: DataTypes.DECIMAL(3, 2),
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      tags: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      imgToUrl: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      phoneNumber: DataTypes.STRING(15),
+      information: DataTypes.STRING(255),
+      homepageUrl: DataTypes.STRING(255),
     },
     {
       sequelize,
       modelName: "Cafe",
-      tableName: "Cafes",
-      timestamps: true,
-    }
+      tableName: "cafes",
+      timestamps: true, 
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",    }
   );
   return Cafe;
 };
