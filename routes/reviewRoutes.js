ae425f5dfa96ce45f61ef47c4a81e20f83a8a4bf
Jihoon Chae <hoonn94@naver.com>
2023-06-21 00:28:20 +0900

    feat : review 라우트 구현

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const reviewController = require("../controllers/reviewController");
+
+router.get("/", reviewController.getAllReviews);
+router.get("/:id", reviewController.getReview);
+router.post("/", reviewController.createReview);
+router.patch("/:id", reviewController.updateReview);
+router.delete("/:id", reviewController.deleteReview);
+
+module.exports = router;
